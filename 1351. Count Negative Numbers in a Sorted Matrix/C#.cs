fb726b1a11a6d133511b97863eb7626e456b7d4b
Will <70348218+imaginary-png@users.noreply.github.com>
2023-05-22 20:37:55 +1000

    add: use binary search

--- 1351. Count Negative Numbers in a Sorted Matrix/C#.cs
+++ 1351. Count Negative Numbers in a Sorted Matrix/C#.cs
@@ -1,15 +1,27 @@
 public class Solution {
     public int CountNegatives(int[][] grid) {
-     var count = 0;
-     for (int i = 0; i < grid.Length; i++)
-     {
-         for (int j = grid[i].Length-1; j >= 0; j--)
-         {
-            if (grid[i][j] >= 0)
-                break;
-            count++;
-         }
-     }
-     return count;
+    var count = 0;
+    for (int i = 0; i < grid.Length; i++)
+    {        
+        var len = grid[i].Length;
+        var left = 0;        
+        var right = len-1;
+         
+        if (grid[i][0] < 0) 
+        {
+             count+= len;
+             continue;
+        }
+        if (grid[i][len-1] >= 0 ) continue;
+
+        while (left <= right)
+        {
+            var mid = left + (right-left)/2;
+            if (grid[i][mid] >= 0) left = mid+1;
+            else right = mid-1;
+        }                
+        count += len-left;
+    }
+    return count;
     }
 }
--- 1351. Count Negative Numbers in a Sorted Matrix/README.md
+++ 1351. Count Negative Numbers in a Sorted Matrix/README.md
@@ -1,4 +1,10 @@
 # Count Negative Numbers in a Sorted Matrix
 ## Easy
 ### https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
-#### O(m n)
+#### O(m n) / O(mlogn)
+
+iterate each array starting from end, til > 0 num found
+
+or
+
+binary search and get count from item count - found index
