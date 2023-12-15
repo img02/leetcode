b6dc2bc37e449ce5f38f280cd49e67a7d13a454d
Will <70348218+img02@users.noreply.github.com>
2023-12-15 17:04:28 +1100

    add o(1) extra space complexity solution

--- 238. Product of Array Except Self/C#.cs
+++ 238. Product of Array Except Self/C#.cs
@@ -1,29 +1,54 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-       
-       var prefix = new int[nums.Length];
-       var postfix = new int[nums.Length];
+        // product of all excluding curreng
+        // is the result of pre and post values of index
+
+        var prefix = new int[nums.Length];
+        prefix[0] = 1;
+        var postfix = new int[nums.Length];
+        postfix[postfix.Length-1] = 1;
+
+        var results = new int[nums.Length];
+        results[0] = 1;
+
+        for (var i = 1; i < nums.Length; i++){
+            prefix[i] = nums[i-1]*prefix[i-1];
+        }
+
+        for (var i = nums.Length-2; i >= 0; i--){
+            postfix[i] = nums[i+1]*postfix[i+1];
+        }
+
+        for (var i = 0; i < nums.Length; i++){
+            results[i] = prefix[i]*postfix[i];
+        }
+        return results;
+        
+    }
+}
 
 
-       prefix[0] = 1;
-       postfix[nums.Length-1] = 1;
-    
-       for (int i = 0, j = nums.Length-1; i < nums.Length && j >= 0; i++, j--)
-       {
-           if (i > 0) prefix[i] = prefix[i-1] * nums[i-1];
+// o(1) space complexity
 
-           if (j < nums.Length-1) postfix[j] = postfix[j+1] * nums[j+1];
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        // product of all excluding curreng
+        // is the result of pre and post values of index
+		
+        var results = new int[nums.Length];
 
-           //Console.WriteLine($"{prefix[i]}|{postfix[j]}");
-       }
+        var prefix = 1;
+        for (var i = 0; i < nums.Length; i++){
+            results[i] = prefix; // store prefix 
+            prefix *= nums[i]; // update prefix product for next loop,
+			// product of prev values
+        }
+        var postfix = 1;
+        for (var i = nums.Length -1; i >= 0; i--){ // reverse
+            results[i] *= postfix; // update results, prefix * postfix
+            postfix *= nums[i]; // update postfix for next loop,  postfix[i] = nums[i+1]* postfix[i+1]
+        }
 
-       var result = new int[nums.Length];      
-
-       for (int i = 0; i < result.Length; i++){
-           result[i] = prefix[i] * postfix[i];
-       }
-
-       return result;
-
+        return results;        
     }
 }
