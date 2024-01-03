f61a271e19599be8b61fa58bfb670b023b994608
Will <70348218+img02@users.noreply.github.com>
2024-01-03 16:47:30 +1100

    add new solution

--- 20 Valid Parentheses/C#.cs
+++ 20 Valid Parentheses/C#.cs
@@ -30,3 +30,27 @@ public class Solution {
         return count == 0;        
     }
 }
+
+// solution 2
+public class Solution {
+    public bool IsValid(string s) {
+        var braces = new Dictionary<char,char>(){
+            {'(',')'},
+            {'{','}'},
+            {'[',']'}
+        };
+
+        var needed = new Stack<char>();
+
+        foreach( var c in s){            
+            if (braces.ContainsKey(c)) {
+                needed.Push(braces[c]);
+                continue;
+            }
+            if (needed.Count > 0 && needed.Peek() == c) needed.Pop();
+            else return false;
+        }
+
+        return needed.Count == 0;
+    }
+}
