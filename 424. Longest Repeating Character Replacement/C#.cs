8289d480fd69aa9cea8be6c2bdb36903a328b657
Will <70348218+img02@users.noreply.github.com>
2025-02-22 10:35:21 +1100

    update

--- 424. Longest Repeating Character Replacement/C#.cs
+++ 424. Longest Repeating Character Replacement/C#.cs
@@ -1,28 +1,55 @@
 public class Solution {
     public int CharacterReplacement(string s, int k) {
-        var longest = 1;
-        var maxFreq = 0;
-        var map = new Dictionary<char,int>();
+        var letterCount = new Dictionary<char,int>();
+        var longest = 0;
+        var mostFreq = 0;
 
         var left = 0;
         var right = 0;
-        while(right < s.Length ){            
+        while (right < s.Length){
             var c = s[right];
-            if (!map.ContainsKey(c)) map[c] = 0;
-            map[c]++;
+            //updating letter count
+            if(!letterCount.ContainsKey(c)) letterCount.Add(c,0);
+            letterCount[c]++;
+
+            if(letterCount[c] > mostFreq) mostFreq = letterCount[c];
             
-            if(map[c] > maxFreq) maxFreq = map[c];
-            // total letters, minus max repeating letter, if remaining to be changed is greater than k possible changes
-            if ( (right-left+1 - maxFreq ) > k) {
-                map[s[left]]--;
-                left++;
-            }
+            //check if replacables exceed k
+            var substringLength = right - left + 1;
+            var replaceables = substringLength - mostFreq;
+            
+            // dont need to check and update letter count / most freq here
+            // because regardless of most freq being no longer accurate
+            // right point moves up as well as left -- sliding window
+            // so replaceables count will be larger than k
+            // until old mostFreq is exceeded by a new substring
+            if (replaceables > k){
+                letterCount[s[left]]--;
+                left++;              
+            }            
             else {
-                longest = Math.Max(longest, right - left + 1);
+            longest = Math.Max(longest, substringLength);
             }
-                right++;
+
+            right++;
         }
 
-        return longest;
+        return longest;        
     }
 }
+
+// substring
+
+// (single char - most)(different chars count == k)
+
+// most frequent character (main char) dictionary<char, int> letter, count
+// lenght of the usbstring - most frequent count = number of replacable characters
+
+// if replacable count > k
+// then currently substring maxed out
+
+// reduce the [LEFT] letters count 
+// Update the most frequent char, if updated count is less than mostfrequent
+// move left pointer up
+
+// repeat until replaceableCount <= k
