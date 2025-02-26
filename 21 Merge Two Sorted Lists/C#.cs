ce09de16185f4f236c658f41edebedb6b2e59ac0
Will <70348218+img02@users.noreply.github.com>
2025-02-27 08:45:18 +1100

    update

--- 21 Merge Two Sorted Lists/C#.cs
+++ 21 Merge Two Sorted Lists/C#.cs
@@ -10,62 +10,47 @@
  * }
  */
 public class Solution {
-    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {       
-       var mergedList = new ListNode();
-       var head = mergedList;
-       Recursive(list1, list2, mergedList);
-       return head.next;
-       //return Iterative(list1, list2);
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        //return Iterative(list1, list2);
+        return Recursive(list1, list2, new ListNode());
     }
 
-    private void Recursive(ListNode list1, ListNode list2, ListNode mergedList) {
-        if (list1 == null && list2 == null) return;
-        if (list1 == null){
-            mergedList.next = list2;
-            return;
+    public ListNode Recursive(ListNode list1, ListNode list2, ListNode merged){
+       
+         if(list1 != null && (list2 == null || list1.val <= list2.val)){
+            merged.next = list1;
+            Recursive(list1.next, list2, merged.next);
         }
-        if (list2 == null){
-            mergedList.next = list1;
-            return;
+        else if (list2 != null){
+            merged.next = list2;
+            Recursive(list1, list2.next, merged.next);
         }
-
-        if (list1.val <= list2.val)
-        {
-            mergedList.next = list1;
-            mergedList = mergedList.next;
-            list1 = list1.next;
-        } 
-        else
-        {
-            mergedList.next = list2;
-            mergedList = mergedList.next;
-            list2 = list2.next;
-        }
-        Recursive(list1, list2, mergedList);
+        return merged.next;
     }
 
-    private ListNode Iterative(ListNode list1, ListNode list2) {
-         if (list1 == null && list2 == null) return null;        
-        var head = new ListNode();             
-        var curr = head;
+    public ListNode Iterative(ListNode list1, ListNode list2){
 
-        while (list1 != null && list2 != null)
-        {
-            if (list1.val <= list2.val)
-            {
-                curr.next = list1;
-                curr = curr.next;
+        var dummy = new ListNode(0,null);
+        var merged = dummy;
+
+        while(list1 != null && list2 != null){
+
+            if(list1.val <= list2.val){
+                merged.next = list1;
                 list1 = list1.next;
-            } 
-            else
-            {
-                curr.next = list2;
-                curr = curr.next;
+            }
+            else {
+                merged.next = list2;
                 list2 = list2.next;
             }
+            merged = merged.next;
+           
+
         }
-        if (list1 == null) curr.next = list2;
-        if (list2 == null) curr.next = list1;        
-        return head.next;
+
+        if(list1 == null) merged.next = list2;
+        else merged.next = list1;
+
+        return dummy.next;
     }
 }
